31045b79b0a93faa373c421d874b8c1847476286
Pavlo Pidlepynskyi <pidlepynskyi-portfolio@ukr.net>
2026-09-10 17:58:07 +0300

    feat: add Category entity with self-referencing hierarchy

---
+++ src/StockFlow.Domain/Entities/Category.cs
@@ -0,0 +1,27 @@
+﻿using StockFlow.Domain.Common;
+
+namespace StockFlow.Domain.Entities
+{
+    public class Category : Entity<Guid>
+    {
+        public string Name { get; private set; }
+        public Guid? ParentCategoryId { get; private set; }
+
+        private Category(Guid id, string name, Guid? parentCategoryId)
+            : base(id)
+        {
+            Name = name;
+            ParentCategoryId = parentCategoryId;
+        }
+
+        public static Category Create(string name, Guid? parentCategoryId)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Category name cannot be empty", nameof(name));
+            }
+
+            return new Category(Guid.NewGuid(), name, parentCategoryId);
+        }
+    }
+}
