30f0490e88e0282adacc48e2f6d489490b84076d
Pavlo Pidlepynskyi <pidlepynskyi-portfolio@ukr.net>
2026-09-12 23:16:54 +0300

    test: add PurchaseOrderLine.Create valid case (Green)

---
+++ src/StockFlow.Domain/Entities/PurchaseOrderLine.cs
@@ -0,0 +1,23 @@
+﻿namespace StockFlow.Domain.Entities
+{
+    public class PurchaseOrderLine
+    {
+        public Guid ProductId { get; }
+        public int QuantityOrdered { get; }
+        public int QuantityReceived { get; private set; }
+        public decimal UnitPrice { get; }
+
+        private PurchaseOrderLine(Guid productId, int quantityOrdered, decimal unitPrice)
+        {
+            ProductId = productId;
+            QuantityOrdered = quantityOrdered;
+            UnitPrice = unitPrice;
+            QuantityReceived = 0;
+        }
+
+        public static PurchaseOrderLine Create(Guid productId, int quantityOrdered, decimal unitPrice)
+        {
+            return new PurchaseOrderLine(productId, quantityOrdered, unitPrice);
+        }
+    }
+}
---
+++ tests/StockFlow.Domain.UnitTests/Entities/PurchaseOrderLineTests.cs
@@ -0,0 +1,20 @@
+﻿using StockFlow.Domain.Entities;
+
+namespace StockFlow.Domain.UnitTests.Entities
+{
+    public class PurchaseOrderLineTests
+    {
+        [Fact]
+        public void Create_WithValidData_ShouldSucceed()
+        {
+            var productId = Guid.NewGuid();
+
+            var line = PurchaseOrderLine.Create(productId, quantityOrdered: 10, unitPrice: 25.50m);
+
+            Assert.Equal(productId, line.ProductId);
+            Assert.Equal(10, line.QuantityOrdered);
+            Assert.Equal(0, line.QuantityReceived);
+            Assert.Equal(25.50m, line.UnitPrice);
+        }
+    }
+}
